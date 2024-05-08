4a43e3405e408fdd42501fee8892dd239376b295
hirankaush <hiran.private.2x@gmail.com>
2024-05-08 22:03:36 +0530

    add GetAllPayments route

--- routes/PaymentRoute.js
+++ routes/PaymentRoute.js
@@ -3,5 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/checkout', paymentController.checkOutController);
+router.get('/getAllPayments', paymentController.getAllPayments);
+
 
 module.exports = router;
